2990eea2aedc88bfae5260010893645cd64c8473
Student <Student@LabVM2049939.cfm5j5plkdnupicr0kbjqdaane.jnbx.internal.cloudapp.net>
2026-09-13 12:47:36 +0200

    Initial file commit

---
+++ src/main/java/com/mycompany/mainchatapp/MainChatApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mainchatapp;
+
+/**
+ *
+ * @author Student
+ */
+public class MainChatApp {
+
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ src/test/java/com/mycompany/mainchatapp/LoginClassTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.mainchatapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class LoginClassTest {
+    
+    public LoginClassTest() {
+        
+    }
+
+    
+}

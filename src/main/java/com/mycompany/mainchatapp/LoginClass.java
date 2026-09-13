aadc3adafefb5763288b0152c1c26a76a4d74da3
Student <Student@LabVM2049939.cfm5j5plkdnupicr0kbjqdaane.jnbx.internal.cloudapp.net>
2026-09-13 21:54:15 +0200

    implement username and password validation methods implement cell phone regex validation implement status message to confirm registration

--- src/main/java/com/mycompany/mainchatapp/LoginClass.java
+++ src/main/java/com/mycompany/mainchatapp/LoginClass.java
@@ -26,10 +26,96 @@ public LoginClass(String username, String password, String phoneNumber,
         this.lastName = lastName;
     }
 
-//method to check username
-//method to check password complexity rules
-//method to check phone number with regex
-//method ro register user
-//method to verify login
-}
+/*method to check username:
+    username must contain an underscore and must not be more than 5 characters long
+*/
+    public boolean checkUserName(String username){
+        if(username == null){ //rejects null username
+            return false;
+        }
+      return username.contains("_")  && username.length() <=5; //must contain an underscore and be at most 5 characters long
+    }
+
+/*method to check password complexity rules with regex:
+    password should be at least 8 char long, have a capital letter, a number and a special character
+*/  
+    public boolean checkPasswordComplexity (String password) {
+//rejects null password
+        if (password == null) {
+            return false;
+        }
+
+//rejects password shorter than 8 characters
+       if (password.length() < 8 ) {
+            return false;
+        }
+       
+       //use regex to ensure capital letter, number and special character
+       boolean hasCapital = password.matches(".*[A-Z].*"); // password has at least one capital letter
+       boolean hasNumber = password.matches(".*[0-9].*"); // password contains at least one number
+       boolean hasSpecialChar = password.matches (".*[^a-zA-Z0-9\\s].*"); // password contains at least one special character (not a letter, digit or whitespace)
+       
+       //combining all checks
+       return hasCapital && hasNumber && hasSpecialChar;
+    }
+
+/*method to check phone number with regex:
+   it should have the international country code and number
+*/
+    public boolean checkPhoneNumber(String phoneNumber) {
+        
+       return phoneNumber != null && phoneNumber.matches("^\\+27\\d{9}$") ; //this ensures it has RSA's +27 code and 9 numbers that follow
+    }
+            
+            
+/*method to register user:
+    must return a message if:
+    -The username is incorrectly formatted
+    -The password does not meet the complexity requirements
+    -The two above conditions have been met, and the user has been registered successfully. 
+*/
+    public String registerUser() {
+      boolean isUsernameValid = checkUserName(username);
+      boolean isPasswordValid = checkPasswordComplexity(password);
+      boolean isCellPhoneValid = checkPhoneNumber(phoneNumber);
+      
+      if (isUsernameValid == false) {
+          return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length";     
+      }
+      
+      if (isPasswordValid == false) {
+          return "Password is not correctly formatted. Please ensure that the password contains at least eight characters, a capital letter, a number and a special character";
+      }
+      
+      if (isCellPhoneValid == false) {
+          return "Cell phone number incorrectly formatted or does not contain international code";   
+      }
+      
+      return "User registered successfully";
+    }
+    
+/*method to verify login:
+   verifies that the login details entered match the login details stored when the user registers
+*/
+   public boolean  loginUser(String username, String password) {
+       if (username == null) {
+        return false;
+    }
+       if (password == null) {
+        return false;
+    }
+       
+       return username.equals(this.username) && password.equals(this.password);   
+   }
+   
+/* method to display message for successful/failed login:
+*/
+   public String returnLoginStatus(boolean isLoggedIn) {
+       if(isLoggedIn) {
+           return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+       } else {
+            return "Username or password incorrect, please try again";   
+               }    
+       }
+   }
 

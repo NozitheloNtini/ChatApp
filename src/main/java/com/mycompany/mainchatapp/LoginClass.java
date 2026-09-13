cd8dfcc19d9fabb35d99372a40fe37f08f404738
Student <Student@LabVM2049939.cfm5j5plkdnupicr0kbjqdaane.jnbx.internal.cloudapp.net>
2026-09-13 13:28:47 +0200

    created Login class skeleton for upcoming methods

--- src/main/java/com/mycompany/mainchatapp/LoginClass.java
+++ src/main/java/com/mycompany/mainchatapp/LoginClass.java
@@ -9,5 +9,27 @@ package com.mycompany.mainchatapp;
  * @author Student
  */
 public class LoginClass {
-    
+//variables to store user details
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;    
+
+//creating a constructor with parameters
+public LoginClass(String username, String password, String phoneNumber,
+                 String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+//method to check username
+//method to check password complexity rules
+//method to check phone number with regex
+//method ro register user
+//method to verify login
 }
+
